fe852ec69f835317615d5dbde3ad1651e9cc4d87
CZH\haripriya.harikrishnan <haripriya.harikrishnan@bshg.com>
2021-01-12 09:51:14 +0530

    commiting

--- src/hari_sample/globalVars.groovy
+++ src/hari_sample/globalVars.groovy
@@ -2,7 +2,7 @@ package hari_sample
 
 class GlobalVars {
    static String foo = "bar"
-
+   static String foo1 = "bar1"
    // refer to this in a pipeline using:
    // import hari_sample
    // println GlobalVars.foo
